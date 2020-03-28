a705a93e797306c23e98932a546739add14c293c
Victor Cardona <victor.cardona@icloud.com>
2020-03-28 14:50:55 -0500

    Initial commit

---
+++ src/main/java/com/marvic/popstats/PopStatsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.marvic.popstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopStatsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopStatsJavaApplication.class, args);
+    }
+
+}
